0b4244791e4d609c996224b43d4bb8464930fb85
Abhishek Singh <53123915+abhishi1432@users.noreply.github.com>
2021-09-03 16:56:34 +0530

    Update my_scrapper.py

--- my_scrapper.py
+++ my_scrapper.py
@@ -5,8 +5,8 @@ from selenium.webdriver.support.select import Select
 import pandas as pd
 
 auton = webdriver.ChromeOptions()
-#auton.add_argument("headless");
-driver = webdriver.Chrome(options=auton, executable_path="C:/Users/Abhishek/Desktop/chromedriver_win32/chromedriver.exe")
+auton.add_argument("headless");
+driver = webdriver.Chrome(options=auton, executable_path="C:/Users/Abhishek/Desktop/chromedriver.exe")
 
 def getSGPA(roll):
     try:
@@ -14,7 +14,7 @@ def getSGPA(roll):
         driver.find_element_by_id('txtrollno').send_keys(roll)
         driver.find_element_by_id('btnSearch').click()
         sl = Select(driver.find_element_by_id('ddlResult'))
-        sl.select_by_visible_text('REGULAR (2019-20) Semester 3-4')
+        sl.select_by_visible_text('REGULAR (2020-21) Semester 5-6')
         driver.find_element_by_id('btnGo').click()
         obtmrk = driver.find_element_by_id('lbltotlmarksDisp').text
         name = driver.find_element_by_id('lblname').text
@@ -29,19 +29,26 @@ def main():
     names=[]
     SGPA=[]
     roll=[]
-    for i in range(18401, 18470):
+    focus=[]
+    lateral=[y for y in range(198601,198608)]
+    regular=[x for x in range(18601,18667)]
+    regular.extend(lateral)
+    focus.extend(regular)
+    for i in focus:
         temp = getSGPA(i)
-        names.append(temp[1])
-        SGPA.append(temp[0])
-        roll.append(i)
+        if(temp[1] != "NA"):
+            names.append(temp[1])
+            SGPA.append(temp[0])
+            roll.append(i)
         driver.execute_script("window.scrollTo(0,200)")
-        if(input("To see next press Enter : ")==""):
-            driver.back()
-            print("Currently Displaying :",i+1)
-            continue
+        # if(input("To see next press Enter : ")==""):
+        #     driver.back()
+        #     print("Currently Displaying :",i+1)
+        #     continue
+        driver.back()
     dct={"Roll No.": pd.Series(roll).astype(int),"Name":pd.Series(names),"Marks/SGPA":pd.Series(SGPA)}
     df=pd.DataFrame(dct);
-    df.to_csv('Result.csv',index=False)
+    df.to_csv('Result_IT.csv',index=False)
     print("DONE")
 
 if __name__ == "__main__":
