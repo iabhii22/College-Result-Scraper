1e6ed80d59215ccd280362f9e01093ff731f0926
Abhishek Singh <53123915+abhishi1432@users.noreply.github.com>
2021-10-08 10:27:10 +0530

    Update my_scrapper.py

--- my_scrapper.py
+++ my_scrapper.py
@@ -16,29 +16,48 @@ def getSGPA(roll):
         sl = Select(driver.find_element_by_id('ddlResult'))
         sl.select_by_visible_text('REGULAR (2020-21) Semester 5-6')
         driver.find_element_by_id('btnGo').click()
-        obtmrk = driver.find_element_by_id('lbltotlmarksDisp').text
+        obtmrk = driver.find_element_by_id('lblsgpaodddisp').text
+        obtmark2=driver.find_element_by_id('lblsgpaevendisp').text
+        obtmark3=driver.find_element_by_id('lbltotlmarksDisp').text
         name = driver.find_element_by_id('lblname').text
-        return [obtmrk,name]
+        return [obtmrk,obtmark2,obtmark3,name]
     except:
         print("Roll number ", roll, ' not available')
         return ["NA","NA"]
 
 def main():
+
+
+    ##### BRANCH SELECTION ######
+    print("ENTER YOUR BRANCH CODE- ")
+    print("CE:1   CS:2   EE:3   EL:4  ME:5   IT:6")
+    branch_code = int(input())
+    ########################
+    
+    
     print("Fetching Result...")
     driver.get('https://govexams.com/knit/searchresult.aspx')
     names=[]
-    SGPA=[]
+    SGPA_odd=[]
+    SGPA_even=[]
+    CGPA=[]
     roll=[]
     focus=[]
-    lateral=[y for y in range(198601,198608)]
-    regular=[x for x in range(18601,18667)]
+    lim1=18000 + branch_code*100 + 1
+    lim2=18000 + branch_code*100 + 70
+    lim3=198000 + branch_code*100 + 1
+    lim4=198000 + branch_code*100 + 8
+    lateral=[y for y in range(lim3,lim4)]
+    regular=[x for x in range(lim1,lim2)]
     regular.extend(lateral)
     focus.extend(regular)
     for i in focus:
         temp = getSGPA(i)
         if(temp[1] != "NA"):
-            names.append(temp[1])
-            SGPA.append(temp[0])
+            names.append(temp[3])
+            SGPA_odd.append(temp[0])
+            SGPA_even.append(temp[1])
+            CGPA.append(temp[2])
             roll.append(i)
         driver.execute_script("window.scrollTo(0,200)")
         # if(input("To see next press Enter : ")==""):
@@ -46,9 +65,10 @@ def main():
         #     print("Currently Displaying :",i+1)
         #     continue
         driver.back()
-    dct={"Roll No.": pd.Series(roll).astype(int),"Name":pd.Series(names),"Marks/SGPA":pd.Series(SGPA)}
+    dct={"Roll No.": pd.Series(roll).astype(int),"Name":pd.Series(names),"SGPA_odd":pd.Series(SGPA_odd),"SGPA_even":pd.Series(SGPA_even),"CGPA":pd.Series(CGPA)}
     df=pd.DataFrame(dct);
-    df.to_csv('Result_IT.csv',index=False)
+    file_name='Result_'+str(branch_code)+'.csv'
+    df.to_csv(file_name,index=False)
     print("DONE")
 
 if __name__ == "__main__":
