6be23a00e1faa32aa8ae44540dc35a2350a6388c
Abhishek Singh <53123915+abhishi1432@users.noreply.github.com>
2021-10-10 21:29:12 +0530

    Update my_scrapper.py

--- my_scrapper.py
+++ my_scrapper.py
@@ -16,11 +16,12 @@ def getSGPA(roll):
         sl = Select(driver.find_element_by_id('ddlResult'))
         sl.select_by_visible_text('REGULAR (2020-21) Semester 5-6')
         driver.find_element_by_id('btnGo').click()
-        obtmrk = driver.find_element_by_id('lblsgpaodddisp').text
-        obtmark2=driver.find_element_by_id('lblsgpaevendisp').text
+#         obtmrk = driver.find_element_by_id('lblsgpaodddisp').text
+#         obtmark2=driver.find_element_by_id('lblsgpaevendisp').text
         obtmark3=driver.find_element_by_id('lbltotlmarksDisp').text
         name = driver.find_element_by_id('lblname').text
-        return [obtmrk,obtmark2,obtmark3,name]
+#         return [obtmrk,obtmark2,obtmark3,name]
+        return [obtmark3,name]
     except:
         print("Roll number ", roll, ' not available')
         return ["NA","NA"]
@@ -54,10 +55,12 @@ def main():
     for i in focus:
         temp = getSGPA(i)
         if(temp[1] != "NA"):
-            names.append(temp[3])
-            SGPA_odd.append(temp[0])
-            SGPA_even.append(temp[1])
-            CGPA.append(temp[2])
+#             names.append(temp[3])
+            names.append(temp[1])
+#             SGPA_odd.append(temp[0])
+#             SGPA_even.append(temp[1])
+#             CGPA.append(temp[2])
+            CGPA.append(temp[0])
             roll.append(i)
         driver.execute_script("window.scrollTo(0,200)")
         # if(input("To see next press Enter : ")==""):
@@ -65,7 +68,8 @@ def main():
         #     print("Currently Displaying :",i+1)
         #     continue
         driver.back()
-    dct={"Roll No.": pd.Series(roll).astype(int),"Name":pd.Series(names),"SGPA_odd":pd.Series(SGPA_odd),"SGPA_even":pd.Series(SGPA_even),"CGPA":pd.Series(CGPA)}
+#     dct={"Roll No.": pd.Series(roll).astype(int),"Name":pd.Series(names),"SGPA_odd":pd.Series(SGPA_odd),"SGPA_even":pd.Series(SGPA_even),"CGPA":pd.Series(CGPA)}
+    dct={"Roll No.": pd.Series(roll).astype(int),"Name":pd.Series(names),"CGPA":pd.Series(CGPA)}
     df=pd.DataFrame(dct);
     file_name='Result_'+str(branch_code)+'.csv'
     df.to_csv(file_name,index=False)
