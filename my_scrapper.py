8be051b137dbe45ff1bda92b7d154837a30d73bf
Abhishek Singh <53123915+abhishi1432@users.noreply.github.com>
2021-10-11 19:20:44 +0530

    Update my_scrapper.py
    bug resolved
    added more flexibility in getting result

--- my_scrapper.py
+++ my_scrapper.py
@@ -1,79 +1,130 @@
-#Refer Readme file for setup instruction
+# Refer Readme file for setup instruction
 
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 import pandas as pd
 
 auton = webdriver.ChromeOptions()
-auton.add_argument("headless");
-driver = webdriver.Chrome(options=auton, executable_path="C:/Users/Abhishek/Desktop/chromedriver.exe")
+auton.add_argument("headless")
 
-def getSGPA(roll):
+###############  Change chromedriver.exe path here  #############
+driver = webdriver.Chrome(options=auton, executable_path="C:/Users/Abhishek/Desktop/chromedriver.exe")
+#################################################################
+flag=1
+def getSGPA(roll,str_ing,sems):
+    global flag
     try:
         driver.find_element_by_id('txtrollno').clear()
         driver.find_element_by_id('txtrollno').send_keys(roll)
         driver.find_element_by_id('btnSearch').click()
         sl = Select(driver.find_element_by_id('ddlResult'))
-        sl.select_by_visible_text('REGULAR (2020-21) Semester 5-6')
+        sl.select_by_visible_text(str_ing)
         driver.find_element_by_id('btnGo').click()
-#         obtmrk = driver.find_element_by_id('lblsgpaodddisp').text
-#         obtmark2=driver.find_element_by_id('lblsgpaevendisp').text
-        obtmark3=driver.find_element_by_id('lbltotlmarksDisp').text
+        try:
+            obtmrk = driver.find_element_by_id('lblsgpaodddisp').text
+        except:
+            obtmrk=-1
+        try:
+            obtmark2=driver.find_element_by_id('lblsgpaevendisp').text
+        except:
+            obtmark2=-1
+        obtmark3 = driver.find_element_by_id('lbltotlmarksDisp').text
         name = driver.find_element_by_id('lblname').text
-#         return [obtmrk,obtmark2,obtmark3,name]
-        return [obtmark3,name]
+        return [obtmrk,obtmark2,obtmark3,name]
     except:
-        print("Roll number ", roll, ' not available')
-        return ["NA","NA"]
-
-def main():
+        print("RESULT OF", roll, "FOR SEM-",sems,'not available')
+        return ["NA", "NA"]
 
 
-    ##### BRANCH SELECTION ######
+##################################### MAIN FUNCTION BEGINS HERE ###############################
+while(True):
     print("ENTER YOUR BRANCH CODE- ")
     print("CE:1   CS:2   EE:3   EL:4  ME:5   IT:6")
     branch_code = int(input())
-    ########################
-    
-    
-    print("Fetching Result...")
-    driver.get('https://govexams.com/knit/searchresult.aspx')
-    names=[]
-    SGPA_odd=[]
-    SGPA_even=[]
-    CGPA=[]
-    roll=[]
-    focus=[]
-    lim1=18000 + branch_code*100 + 1
-    lim2=18000 + branch_code*100 + 70
-    lim3=198000 + branch_code*100 + 1
-    lim4=198000 + branch_code*100 + 8
-    lateral=[y for y in range(lim3,lim4)]
-    regular=[x for x in range(lim1,lim2)]
-    regular.extend(lateral)
-    focus.extend(regular)
-    for i in focus:
-        temp = getSGPA(i)
-        if(temp[1] != "NA"):
-#             names.append(temp[3])
-            names.append(temp[1])
-#             SGPA_odd.append(temp[0])
-#             SGPA_even.append(temp[1])
-#             CGPA.append(temp[2])
-            CGPA.append(temp[0])
-            roll.append(i)
-        driver.execute_script("window.scrollTo(0,200)")
-        # if(input("To see next press Enter : ")==""):
-        #     driver.back()
-        #     print("Currently Displaying :",i+1)
-        #     continue
-        driver.back()
-#     dct={"Roll No.": pd.Series(roll).astype(int),"Name":pd.Series(names),"SGPA_odd":pd.Series(SGPA_odd),"SGPA_even":pd.Series(SGPA_even),"CGPA":pd.Series(CGPA)}
-    dct={"Roll No.": pd.Series(roll).astype(int),"Name":pd.Series(names),"CGPA":pd.Series(CGPA)}
-    df=pd.DataFrame(dct);
-    file_name='Result_'+str(branch_code)+'.csv'
-    df.to_csv(file_name,index=False)
-    print("DONE")
+    if branch_code < 1 or branch_code > 6:
+        print("WRONG CHOICE ENTERED! TRY AGAIN")
+    else:
+        break
+branch_to_code={1:"CE",2:"CS",3:"EE",4:"EL",5:"ME",6:"IT"}
 
-if __name__ == "__main__":
-    main()
+
+####### BATCH SELECTION############
+while (True):
+    print("ENTER YOUR BATCH( Ex: 2018-22 )")
+    year = input()
+    start_year = int(year[2:4])
+    end_year = int(year[5:])
+    if end_year-start_year != 4:
+        print('WRONG VALUE ENTERED! TRY AGAIN')
+    else:
+        break
+
+
+######## SEMESTER SELECTION #########
+while(True):
+    print("ENTER SEMESTER(should be a single digit number)-")
+    sem=int(input())
+    if sem < 1 or sem > 8:
+        print("WRONG CHOICE ENTERED! TRY AGAIN")
+    else:
+        break
+semester={1:"1-2",2:"1-2",3:"3-4",4:"3-4",5:"5-6",6:"5-6",7:"7-8",8:"7-8"}
+
+
+########DON'T TOUCH THIS ###############################
+if sem == 1 or sem == 2:
+    s= str(2000+ start_year) + "-" + str(start_year+1)
+elif sem==3 or sem==4 :
+    s = str(2000 + start_year+1) + "-" + str(start_year + 2)
+elif sem==5 or sem==6:
+    s = str(2000 + start_year+2) + "-" + str(start_year + 3)
+elif sem== 7 or sem == 8:
+    s = str(2000 + start_year+3) + "-" + str(start_year + 4)
+###########################################################
+
+
+driver.get('https://govexams.com/knit/searchresult.aspx')
+names = []
+SGPA_odd = []
+SGPA_even = []
+CGPA = []
+roll = []
+roll_list = []
+
+######### ROLL NO LIST GENERATION ###########
+regular_start = start_year*1000 + branch_code * 100 + 1
+regular_end = start_year*1000 + branch_code * 100 + 75
+lateral_start = (start_year+1)*10000 + (start_year%10)*1000 + branch_code * 100 + 1
+lateral_end = (start_year+1)*10000 + (start_year%10)*1000 + branch_code * 100 + 8
+regular = [y for y in range(regular_start, regular_end)]
+lateral = [x for x in range(lateral_start, lateral_end)]
+regular.extend(lateral)
+roll_list.extend(regular)
+################################################
+
+str_ing='REGULAR (' + s + ') Semester ' + semester[sem]
+
+print("Fetching Result...")
+for i in roll_list:
+    temp = getSGPA(i,str_ing,semester[sem])
+    if (temp[1] != "NA"):
+        names.append(temp[3])
+        SGPA_odd.append(temp[0])
+        SGPA_even.append(temp[1])
+        CGPA.append(temp[2])
+        roll.append(i)
+    # driver.execute_script("window.scrollTo(0,200)")
+    # if(input("To see next press Enter : ")==""):
+    #     driver.back()
+    #     print("Currently Displaying :",i+1)
+    #     continue
+    driver.back()
+
+dct={"Roll No.": pd.Series(roll).astype(int),"Name":pd.Series(names),"SGPA_odd":pd.Series(SGPA_odd),"SGPA_even":pd.Series(SGPA_even),"CGPA":pd.Series(CGPA)}
+df = pd.DataFrame(dct)
+nan_value = float("NaN")
+df.replace(-1, nan_value, inplace=True)
+df.dropna(how='all', axis=1, inplace=True)
+file_name = 'Result_' + str(branch_to_code[branch_code]) + "_" + year + "_"+"Sem."+semester[sem]+ '.csv'
+df.to_csv(file_name, index=False)
+print("DONE")
